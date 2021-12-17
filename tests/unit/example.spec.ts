c99d360182f8eaac5d7522c95910a848296306d4
Jakaria Blaine <jakariablaine120@gmail.com>
2021-12-17 23:12:20 +0600

    Add tests for login page

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,24 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+import Login from "@/views/Login.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+      props: { msg },
+    });
+    expect(wrapper.text()).to.include(msg);
+  });
+});
+
+describe("Login.vue", () => {
+  it("renderes with login input boxes", () => {
+    const wrapper = shallowMount(Login);
+    expect(wrapper.html()).to.include("<input type=\"text\"");
+    expect(wrapper.html()).to.include("input type=\"password\"");
+
+    expect(wrapper.find(".login-btn").text()).to.contain("Login");
+  });
+});
